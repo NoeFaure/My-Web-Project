328c260a4ce171445f3cde2e6624901e43fdb29f
NoeFaure <noe.faure@isep.fr>
2018-09-06 23:21:26 +0200

    Information Container

--- views/articles.php
+++ views/articles.php
@@ -16,6 +16,11 @@
   <div class="col-sm-2"></div>
   <div class="col-sm-8">
 		<ul class="information-container">
+			<li class="information-option-icons">154 <i class="far fa-eye"></i> &nbsp; 32 <i class="fas fa-heart rose"></i></li>
+			<li class="title">Rédaction du feuille de style CSS</li>
+			<li class="line-spacer-information"></li>
+			<li class="under-title">Comment rédiger une feuille de style ?</li>
+			<li class="author">Par : Noé Faure</li>
 		</ul>
 	</div>
   <div class="col-sm-2"></div>
