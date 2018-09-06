2b207cb9dd96786db8cc6273ad8b1d7bf8c8a209
NoeFaure <noe.faure@isep.fr>
2018-09-06 22:03:51 +0200

    1rst steps
    - menu bar
    - css
    - information container

---
+++ layout/menubar.php
@@ -0,0 +1,7 @@
+<ul class="container-fluid menu-bar">
+	<li class="item"><a href="#"><i class="fas fa-language"></i></a></li>
+	<li class="item"><a href="#"><i class="fas fa-newspaper"></i> &nbsp;Articles</a></li>
+  <li class="item"><a href="#"><i class="fas fa-pencil-ruler"></i> &nbsp;Projets</a></li>
+  <li class="item"><a href="#"><i class="fas fa-home"></i> &nbsp;Accueil</a></li>
+  <li class="item logo"><a href="#">Write it</a></li>
+</ul>
--- views/articles.php
+++ views/articles.php
@@ -4,23 +4,22 @@
 <head>
 	<title>Article - </title>
     
-	<link rel="stylesheet" type="text/css" href="../css/article.css">
-	
-	<!-- Bootstrap CDN -->
-	<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css" integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous">
-	<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.min.js" integrity="sha384-ChfqqxuZUCnJSK3+MXmPNIyE6ZbWh2IMqE241rYiqJxyMiZ6OW/JmZQ5stwEULTy" crossorigin="anonymous"></script>
-	<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.bundle.min.js" integrity="sha384-pjaaA8dDz/5BgdFUPX6M/9SUZv4d12SUPF0axWc+VRZkx5xU3daN+lYb49+Ax+Tl" crossorigin="anonymous"></script>
+	<?php require '../layout/assets.php'; ?>
 
 </head>
     
 <body>
 
-<ul class="container-fluid">
-	<li>coucou</li>
-</ul>
+<?php require '../layout/menubar.php'; ?>
 	
-<h1>This is a Heading</h1>
-<p>This is a paragraph.</p>
+<div class="row">
+  <div class="col-sm-2"></div>
+  <div class="col-sm-8">
+		<ul class="information-container">
+		</ul>
+	</div>
+  <div class="col-sm-2"></div>
+</div>
 
 </body>
     
