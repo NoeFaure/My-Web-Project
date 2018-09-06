173a5704d7f92c615f14e07695f8f124e3802d9f
NoeFaure <noe.faure@isep.fr>
2018-09-06 16:23:33 +0200

    Bootstrap
    - Adding bootstrap to the project

---
+++ views/articles.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html>
+    
+<head>
+	<title>Article - </title>
+    
+	<link rel="stylesheet" type="text/css" href="../css/article.css">
+	
+	<!-- Bootstrap CDN -->
+	<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css" integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous">
+	<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.min.js" integrity="sha384-ChfqqxuZUCnJSK3+MXmPNIyE6ZbWh2IMqE241rYiqJxyMiZ6OW/JmZQ5stwEULTy" crossorigin="anonymous"></script>
+	<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.bundle.min.js" integrity="sha384-pjaaA8dDz/5BgdFUPX6M/9SUZv4d12SUPF0axWc+VRZkx5xU3daN+lYb49+Ax+Tl" crossorigin="anonymous"></script>
+
+</head>
+    
+<body>
+
+<ul class="container-fluid">
+	<li>coucou</li>
+</ul>
+	
+<h1>This is a Heading</h1>
+<p>This is a paragraph.</p>
+
+</body>
+    
+</html>
