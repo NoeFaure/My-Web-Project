58acb3e58672954070ecaf492c1fe2014c3fcd98
NoeFaure <noe.faure@isep.fr>
2018-09-08 23:03:55 +0200

    menubar light fix

--- layout/menubar.php
+++ layout/menubar.php
@@ -1,7 +1,7 @@
 <ul class="container-fluid menu-bar">
-	<li class="item"><a href="#"><i class="fas fa-language"></i></a></li>
-	<li class="item"><a href="#"><i class="fas fa-newspaper"></i> &nbsp;Articles</a></li>
-  <li class="item"><a href="#"><i class="fas fa-pencil-ruler"></i> &nbsp;Projets</a></li>
-  <li class="item"><a href="#"><i class="fas fa-home"></i> &nbsp;Accueil</a></li>
-  <li class="item logo"><a href="#">Write it</a></li>
+	<a href="#"><li class="item"><i class="fas fa-language"></i></li></a>
+	<a href="#"><li class="item"><i class="fas fa-newspaper"></i> &nbsp;Articles</li></a>
+	<a href="#"><li class="item"><i class="fas fa-pencil-ruler"></i> &nbsp;Projets</li></a>
+	<a href="#"><li class="item"><i class="fas fa-home"></i> &nbsp;Accueil</li></a>
+	<a href="#"><li class="item logo"><a href="#">Write it</a></li></a>
 </ul>
