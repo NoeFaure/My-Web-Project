0dad722ef433f64d051f3f3f84c050d362ff4ad9
NoeFaure <noe.faure@isep.fr>
2018-09-10 12:25:55 +0200

    Index start

--- layout/menubar.php
+++ layout/menubar.php
@@ -1,7 +1,6 @@
 <ul class="container-fluid menu-bar">
 	<a href="#"><li class="item"><i class="fas fa-language"></i></li></a>
-	<a href="#"><li class="item"><i class="fas fa-newspaper"></i> &nbsp;Articles</li></a>
 	<a href="#"><li class="item"><i class="fas fa-pencil-ruler"></i> &nbsp;Projets</li></a>
-	<a href="#"><li class="item"><i class="fas fa-home"></i> &nbsp;Accueil</li></a>
+	<a href="#"><li class="item"><i class="fas fa-newspaper"></i> &nbsp;Articles</li></a>
 	<a href="#"><li class="item logo"><a href="#">Write it</a></li></a>
 </ul>
--- views/index.php
+++ views/index.php
@@ -1,11 +1,39 @@
 <!DOCTYPE html>
 <html>
 <head>
-<title>Title of the document</title>
+	<?php 
+		require '../layout/assets.php';
+		require_once '../server/db.php';
+		require '../server/functions_article.php';
+		check_geter_article();
+		$id_article = $_GET['article']; $page = $_GET['page']; 
+	?>
+	
+	<title>WriteIt</title>
+	
+	<!-- Feuille de style -->
+	<link rel="stylesheet" type="text/css" href="../css/index.css">
+	
 </head>
 
 <body>
-<h1>INDEX</h1>
+<?php require '../layout/menubar.php'; ?>
+
+<div class="container-fluid">
+	<div class="row main-presentation">
+	</div>
+	<div class="row article-category-container">
+		<div class="article-bullet">Informatique</div>
+	</div>
+	<div class="row">
+  	<div class="col-sm-3 article-container"></div>
+		<div class="col-sm-3"></div>
+		<div class="col-sm-3"></div>
+		<div class="col-sm-3"></div>
+	</div>
+</div>
+	
+<?php require '../layout/footer.php'; ?>
 </body>
 
 </html>
