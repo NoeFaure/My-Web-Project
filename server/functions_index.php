c297bb99ec55743fa96ed5470da9c1c6b77ecda6
NoeFaure <noe.faure@isep.fr>
2018-09-11 21:20:04 +0200

    Loop on index

--- server/functions_index.php
+++ server/functions_index.php
@@ -4,28 +4,52 @@ function browse_article()
 {
 	global $pdo;
 	
-	$req = $pdo->prepare('SELECT * FROM articles');
-	$req->execute();
-	$metadata = $req->fetch();
+	$link = mysqli_connect("localhost", "root", "root", "writeit_db");
+
+	/* Check connection */
+	if (mysqli_connect_errno()) 
+	{
+			printf("Échec de la connexion : %s\n", mysqli_connect_error());
+			exit();
+	}
 	
-	$req2 = $pdo->prepare('SELECT content FROM pages WHERE id_article = ?');
-	$req2->execute([$metadata[0]]);
-	$extract_page = $req2->fetch();
+	$query = "SELECT * FROM articles";
 	
-	$extract_page[0] = substr ($extract_page[0], 0, 700);
+	if ($result = mysqli_query($link, $query)) 
+	{
+
+    /* Récupère un tableau associatif */
+    while ($metadata = mysqli_fetch_assoc($result)) 
+		{
+
 	
-	echo '<a class="article-clickable" href="articles.php?article='. $metadata[0] .'&page=1"><div class="article-container">
-				<div class="article-category">Informatique</div>
-				<div class="article-picture"></div>
-				<div class="article-info-container">
-					<ul class="article-main-info-container">
-						<li class="article-title">' . $metadata[1] .'</li>
-						<li class="article-option rose" data-toggle="tooltip" title="'. $metadata[5] .' likes">&nbsp; '. $metadata[5] .' &nbsp;<i class="fas fa-heart"></i></li>
-						<li class="article-option" data-toggle="tooltip" title="' . $metadata[6] .' vues">&nbsp; ' . $metadata[6] .' &nbsp;<i class="far fa-eye"></i></li>
-						<li class="article-option" data-toggle="tooltip" title="7 follows">7 &nbsp;<i class="fas fa-share"></i>
-					</ul>
-					<div class="article-under-title">' . $metadata[2] .'</div>
-					<div class="article-extract">' . $extract_page[0] . '</div>
-				</div>
-			</div></a>';
+			$req2 = $pdo->prepare('SELECT content FROM pages WHERE id_article = ?');
+			$req2->execute([$metadata['id']]);
+			$extract_page = $req2->fetch();
+
+			$extract_page[0] = substr ($extract_page[0], 0, 700);
+
+			echo '<a class="article-clickable" href="articles.php?article='. $metadata['id'] .'&page=1"><div class="article-container">
+						<div class="article-category">' . $metadata['category'] .'</div>
+						<div class="article-picture"></div>
+						<div class="article-info-container">
+							<ul class="article-main-info-container">
+								<li class="article-title">' . $metadata['title'] .'</li>
+								<li class="article-option rose" data-toggle="tooltip" title="'. $metadata['nb_like'] .' likes">&nbsp; '. $metadata['nb_vues'] .' &nbsp;<i class="fas fa-heart"></i></li>
+								<li class="article-option" data-toggle="tooltip" title="' . $metadata['nb_vues'] .' vues">&nbsp; ' . $metadata['nb_like'] .' &nbsp;<i class="far fa-eye"></i></li>
+								<li class="article-option" data-toggle="tooltip" title="7 follows">7 &nbsp;<i class="fas fa-share"></i>
+							</ul>
+							<div class="article-under-title">' . $metadata['under_title'] .'</div>
+							<div class="article-extract">' . $extract_page[0] . '</div>
+						</div>
+					</div></a>';
+			}
+
+    /* free results */
+    mysqli_free_result($result);
+	}
+
+	/* close connection */
+	mysqli_close($link);
+	
 }
